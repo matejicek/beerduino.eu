b0d55868dad45e0871101940f16326126276fdd5
matejicek <petr.matejicek@endemic.cz>
2013-10-02 10:07:28 +0200

    OK

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("activity");
 		setContentView(R.layout.activity_main);
 	}
 
